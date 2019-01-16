8772ca8156c1272c35ce2801ded9c0a59d964762
Robbie Kruszynski and Travis Toal <robbie@>
2019-01-16 15:55:56 -0800

    add math, break less often

--- js/scripts.js
+++ js/scripts.js
@@ -22,10 +22,43 @@ function logCalc(base, num) {
   return Math.log(num) / Math.log(base);
 };
 
-var displayDig = ["","","","","","","","","","","",""];
+var displayDig = ["", "", "", "", "", "", "", "", "", "", "", ""];
 var i = 0;
+var opClick = "";
+var strSecond = "";
+var number1;
+var number2;
+var digSecond = 0;
 $(document).ready(function() {
   $("button").click(function() {
+    if (opClick != "") {
+      digSecond += 1;
+    }
+
+    if ($(this).text() === "+") {
+      number1 = parseInt(displayDig[0] + displayDig[1] + displayDig[2] + displayDig[3] + displayDig[4] + displayDig[5] + displayDig[6] + displayDig[7] + displayDig[8] + displayDig[9] + displayDig[10] + displayDig[11]);
+      opClick = "+";
+    }
+    else if ($(this).text() === "-") {
+      number1 = parseInt(displayDig[0] + displayDig[1] + displayDig[2] + displayDig[3] + displayDig[4] + displayDig[5] + displayDig[6] + displayDig[7] + displayDig[8] + displayDig[9] + displayDig[10] + displayDig[11]);
+      opClick = "-";
+    }
+    else if ($(this).text() === "*") {
+      number1 = parseInt(displayDig[0] + displayDig[1] + displayDig[2] + displayDig[3] + displayDig[4] + displayDig[5] + displayDig[6] + displayDig[7] + displayDig[8] + displayDig[9] + displayDig[10] + displayDig[11]);
+      opClick = "*";
+    }
+    else if ($(this).text() === "/") {
+      number1 = parseInt(displayDig[0] + displayDig[1] + displayDig[2] + displayDig[3] + displayDig[4] + displayDig[5] + displayDig[6] + displayDig[7] + displayDig[8] + displayDig[9] + displayDig[10] + displayDig[11]);
+      opClick = "/";
+    }
+    else if ($(this).text() === "^") {
+      number1 = parseInt(displayDig[0] + displayDig[1] + displayDig[2] + displayDig[3] + displayDig[4] + displayDig[5] + displayDig[6] + displayDig[7] + displayDig[8] + displayDig[9] + displayDig[10] + displayDig[11]);
+      opClick = "^";
+    }
+    else if ($(this).text() === "log") {
+      number1 = parseInt(displayDig[0] + displayDig[1] + displayDig[2] + displayDig[3] + displayDig[4] + displayDig[5] + displayDig[6] + displayDig[7] + displayDig[8] + displayDig[9] + displayDig[10] + displayDig[11]);
+      opClick = "log";
+    }
     for (i = 0; i < 11; i++) {
       displayDig[i] = displayDig[i+1];
       $("#display div:nth-child(" + (i + 1) + ")").children("h1").text(displayDig[i]);
@@ -40,29 +73,55 @@ $(document).ready(function() {
     };
     displayDig[11] = "";
     $("#display div:nth-child(12)").children("h1").text("");
+    number1 = 0;
+    number2 = 0;
+    opClick = "";
+    strSecond = "";
+    digSecond = 0;
+    $("#answer").text("Here's your answer");
   });
-  $("#submit").click(function(event) {
-    var inputOne = parseInt($("input#input-one").val());
-    var inputTwo = $("#input-two").val();
-    var inputThree = parseInt($("input#input-three").val());
-
-    if (inputTwo === "+") {
-      $("#answer").text(inputOne + " plus " + inputThree + " is " + add(inputOne, inputThree));
+  $("#equals").click(function(event) {
+    if (opClick === "+") {
+      for (i = 0; i < digSecond; i++) {
+        strSecond += displayDig[12-(digSecond - i)];
+      }
+      number2 = parseInt(strSecond);
+      $("#answer").text(add(number1, number2));
     }
-    else if (inputTwo === "-") {
-      $("#answer").text(inputOne + " minus " + inputThree + " is " + subtract(inputOne, inputThree));
+    else if (opClick === "-") {
+      for (i = 0; i < digSecond; i++) {
+        strSecond += displayDig[12-(digSecond - i)];
+      }
+      number2 = parseInt(strSecond);
+      $("#answer").text(subtract(number1, number2));
     }
-    else if (inputTwo === "*") {
-      $("#answer").text(inputOne + " times " + inputThree + " is " + multiply(inputOne, inputThree));
+    else if (opClick === "*") {
+      for (i = 0; i < digSecond; i++) {
+        strSecond += displayDig[12-(digSecond - i)];
+      }
+      number2 = parseInt(strSecond);
+      $("#answer").text(multiply(number1, number2));
     }
-    else if (inputTwo === "/") {
-      $("#answer").text(inputOne + " divided by " + inputThree + " is " + divide(inputOne, inputThree));
+    else if (opClick === "/") {
+      for (i = 0; i < digSecond; i++) {
+        strSecond += displayDig[12-(digSecond - i)];
+      }
+      number2 = parseInt(strSecond);
+      $("#answer").text(divide(number1, number2));
     }
-    else if (inputTwo === "^") {
-      $("#answer").text(inputOne + " raised to the power of " + inputThree + " is " + power(inputOne, inputThree));
+    else if (opClick === "^") {
+      for (i = 0; i < digSecond; i++) {
+        strSecond += displayDig[12-(digSecond - i)];
+      }
+      number2 = parseInt(strSecond);
+      $("#answer").text(power(number1, number2));
     }
-    else if (inputTwo === "log") {
-      $("#answer").text("The logarithm of " + inputThree + " in base " + inputOne + " is " + logCalc(inputOne, inputThree));
+    else if (opClick === "log") {
+      for (i = 0; i < digSecond; i++) {
+        strSecond += displayDig[12-(digSecond - i)];
+      }
+      number2 = parseInt(strSecond);
+      $("#answer").text(logCalc(number1, number2));
     }
     else {
       $("#answer").text("That is not a valid command.");
